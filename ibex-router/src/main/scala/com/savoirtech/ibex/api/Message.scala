14ba980b040c245c5807f14e3dd13a6a23743681
James Carman <jcarman@apache.org>
2014-03-30 18:23:24 -0400

    Adding TODO.

--- ibex-router/src/main/scala/com/savoirtech/ibex/api/Message.scala
+++ ibex-router/src/main/scala/com/savoirtech/ibex/api/Message.scala
@@ -1,5 +1,7 @@
 package com.savoirtech.ibex.api
 
+
+//TODO: Should we consider making the body of type Option[AnyRef] or Option[AnyVal]?  Should we ever allow None body?
 case class Message(body: AnyRef, headers: Map[String, AnyRef] = Map()) {
 
   def copy(newBody: AnyRef) = {
